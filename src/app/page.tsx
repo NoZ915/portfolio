fd63a97f914d8e19e4f45bad816c61e9b0686569
NoZ915 <tina0915tw0915@gmail.com>
2025-04-10 08:29:24 +0800

    feat:Add github link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import SkillSection from "@/components/skills/SkillSection";
 import { ExperienceSection } from "@/components/timeline/ExperienceSection";
+import { Button } from "@/components/ui/button";
+import { FaGithub } from "react-icons/fa";
 
 export default function HomePage() {
   return (
@@ -11,9 +13,15 @@ export default function HomePage() {
           <p className="text-muted-foreground text-md">
             對於前後端開發，有極大的熱情與興趣，總是讓各種專題不再只是數據、想法，而是更具完整性的系統，另外也樂於分享學習新知，曾參與過工作室 JavaScript 讀書會並擔任導讀。
           </p>
+          <Button variant="outline" asChild className="gap-2">
+            <a href="https://github.com/NoZ915" target="_blank" rel="noopener noreferrer">
+              <FaGithub className="text-lg" />
+              NoZ915
+            </a>
+          </Button>
         </div>
 
-        {/* 時間軸 */}
+        {/* 經驗時間軸 */}
         <ExperienceSection />
 
         {/* 技能區塊 */}
