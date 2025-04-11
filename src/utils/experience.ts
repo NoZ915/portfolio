ef35b4e7a4dc1896ccc120baaeb643721cec61ca
NoZ915 <tina0915tw0915@gmail.com>
2025-04-11 22:55:48 +0800

    update:Experience description

--- src/utils/experience.ts
+++ src/utils/experience.ts
@@ -2,24 +2,25 @@ export const experience = [
   // 鈦坦
   {
     year: "2024/07 ~ Now",
-    title: "新加坡商鈦坦科技 軟體實習工程師",
+    title: "新加坡商鈦坦科技｜軟體實習工程師",
     description: [
       {
-        text: "維護與開發超過10項的內部系統",
+        text: "參與並維護超過 10 項內部系統，涵蓋前後端功能優化與使用者體驗提升。",
       },
       {
-        text: "解決掉從古至今所有前人無法解決掉的bug",
+        text: "主動挑戰歷年技術債，成功解決長期未解的疑難 bug。",
       },
       {
-        text: "利用BroadcastChannel API，讓系統可以跨頁同步表單內容，並將知識整理與其他實習生分享",
+        text: "運用 BroadcastChannel API 建立跨分頁資料同步機制，有效提升表單一致性與使用效率，並將解法系統性整理分享給實習生夥伴。",
       },
     ],
   },
 
+
   // 好想工作室
   {
     year: "2021/04 ~ 2023/10",
-    title: "好想工作室 Web Camp",
+    title: "好想工作室 Web Camp | 學員",
     description: [
       {
         text: "每周 JavaScript 讀書會，曾擔任導讀，導讀內容包含閉包與範圍、正規表達式",
