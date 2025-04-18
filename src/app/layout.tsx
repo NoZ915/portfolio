00a2f6111e71d93018cfb3d07ea65aef496fb16f
NoZ915 <tina0915tw0915@gmail.com>
2025-04-18 08:09:40 +0800

    update:favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "YuXuan Lin (林育瑄, Tina Lin)｜NoZ Portfolio",
-  description: "Hi，我是YuXuan Lin (林育瑄, Tina Lin)，這是我以 Next.js 製作的前端開發個人網站與專案展示。",
+  description: "Hi，我是YuXuan Lin (林育瑄, Tina Lin)，這是我的前後端開發個人網站專案展示與個人網站。",
   keywords: [
     "作品集",
     "前端開發",
