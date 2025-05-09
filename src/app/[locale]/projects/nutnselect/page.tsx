a937fc0643f8b6266c89b2e896ede59f9986bbbb
NoZ915 <tina0915tw0915@gmail.com>
2025-05-09 08:38:50 +0800

    feat:Add nutnselect page

---
+++ src/app/[locale]/projects/nutnselect/page.tsx
@@ -0,0 +1,118 @@
+import Image from "next/image";
+import { Badge } from "@/components/ui/badge";
+import { FaCalendarAlt } from "react-icons/fa";
+
+import { projects as projectsZh } from "@/data/projects.zh";
+import { projects as projectsEn } from "@/data/projects.en";
+import { ga as gaZh } from "@/data/ga.zh";
+import { ga as gaEn } from "@/data/ga.en";
+import { useLocale } from "next-intl";
+import { getProjectByLink } from "@/lib/project";
+
+export default function GaPage() {
+  const locale = useLocale();
+  const projects = locale === "zh" ? projectsZh : projectsEn;
+  const project = getProjectByLink(projects, "/projects/nutnselect");
+  const ga = locale === "zh" ? gaZh : gaEn;
+
+  return (
+    <div className="max-w-3xl mx-auto px-6 py-8 space-y-6">
+      <div className="space-y-2">
+        <h1 className="text-3xl font-bold">{project.title}</h1>
+        <span className="flex items-center gap-2 text-sm text-muted-foreground">
+          <FaCalendarAlt className="w-4 h-4" />
+          {project.time}
+        </span>
+        <p className="text-muted-foreground">{project.description}</p>
+      </div>
+
+      <div className="rounded-lg overflow-hidden">
+        <Image
+          src={project.image}
+          alt={`${project.title} image`}
+          width={800}
+          height={400}
+          className="w-full object-cover rounded-lg shadow"
+        />
+      </div>
+
+      {/* 使用技術 */}
+      <div className="space-y-2">
+        <h2 className="text-xl font-semibold">{ga.sections.technologies}</h2>
+        <div className="flex flex-wrap gap-2">
+          {project.skills.map((skill, index) => (
+            <Badge key={index} variant="outline">
+              {skill}
+            </Badge>
+          ))}
+        </div>
+      </div>
+
+      {/* 專案介紹 */}
+      {/* <div className="space-y-2">
+        <h2 className="text-xl font-semibold">{ga.sections.introduction}</h2>
+        <p className="leading-relaxed">
+          {ga.content.intro1}
+        </p>
+        <p className="leading-relaxed">
+          {ga.content.intro2}
+        </p>
+      </div> */}
+
+      {/* 專案連結 */}
+      {/* <div>
+        <h2 className="text-xl font-semibold mb-2">{ga.sections.links}</h2>
+        <ul className="list-disc list-inside space-y-1">
+          <li>
+            <a
+              href="https://ga-exam-item-selection.zeabur.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-600 hover:underline"
+            >
+              {ga.content.linkSite}
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/NoZ915/GA-parallel-test-item-selection?tab=readme-ov-file"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-600 hover:underline"
+            >
+              {ga.content.linkSource}
+            </a>
+          </li>
+        </ul>
+      </div> */}
+
+      {/* 主要功能 */}
+      {/* <div>
+        <h2 className="text-xl font-semibold mb-2">{ga.sections.features}</h2>
+        <ul className="list-disc list-inside space-y-1">
+          {ga.content.features.map((feature, index) => (
+            <li key={index} className="leading-relaxed">
+              {feature}
+            </li>
+          ))}
+        </ul>
+      </div> */}
+
+      {/* 其他補充 */}
+      {/* <div className="mt-6 space-y-6">
+        <h2 className="text-xl font-semibold mb-2">{ga.sections.others}</h2>
+
+        <div>
+          <strong>{ga.content.slides}</strong>
+          <div className="relative w-full pb-[56.25%] mt-2">
+            <iframe
+              src="https://docs.google.com/presentation/d/1bKv9PazLfLrsA85l2YbB4VndDy17RulkHrkfv9y4t0Q/embed?start=false&loop=false&delayms=3000"
+              allowFullScreen
+              className="absolute top-0 left-0 w-full h-full border-0"
+            ></iframe>
+          </div>
+        </div>
+      </div> */}
+    </div>
+  );
+}
